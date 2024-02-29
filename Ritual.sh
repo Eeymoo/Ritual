f0ab156189fc91059941fc92a6a556cf62c834f1
smeb y <48400087+a3165458@users.noreply.github.com>
2024-02-29 18:28:27 +0800

    Update Ritual.sh

--- Ritual.sh
+++ Ritual.sh
@@ -94,6 +94,44 @@ EOF
 
 echo "Config 文件设置完成"
 
+# 安装基本组件
+sudo apt install pkg-config curl build-essential libssl-dev libclang-dev -y
+
+# 检查 Docker 是否已安装
+if ! command -v docker &> /dev/null
+then
+    # 如果 Docker 未安装，则进行安装
+    echo "未检测到 Docker，正在安装..."
+    sudo apt-get install ca-certificates curl gnupg lsb-release
+
+    # 添加 Docker 官方 GPG 密钥
+    sudo mkdir -p /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+    # 设置 Docker 仓库
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    # 授权 Docker 文件
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+    sudo apt-get update
+
+    # 安装 Docker 最新版本
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+else
+    echo "Docker 已安装。"
+fi
+
+# 检查 Docker Compose 是否已安装
+if ! command -v docker-compose &> /dev/null
+then
+    echo "未检测到 Docker Compose，正在安装..."
+    sudo apt install docker-compose -y
+else
+    echo "Docker Compose 已安装。"
+fi
+
 # 启动容器
 docker compose up -d
 
